91c6414549d202607b244701839a373259e8c401
Ángel Vázquez <angel_betta@outlook.com>
2025-12-11 01:09:17 -0600

    Create the `FeedItem` model

---
+++ Sources/EssentialFeed/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+// FeedItem.swift
+// Copyright © 2025 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}

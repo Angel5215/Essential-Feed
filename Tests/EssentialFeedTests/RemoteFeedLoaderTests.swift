18ec0ba656440bbe2f557ce35a2ff9f254504e28
Ángel Vázquez <angel_betta@outlook.com>
2025-12-11 01:32:29 -0600

    Move test logic to a separate type `HTTPClientSpy`

--- Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ Tests/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,28 +9,36 @@ import Testing
 
 class RemoteFeedLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
-@preconcurrency class HTTPClient {
-    static let shared = HTTPClient()
+class HTTPClient {
+    nonisolated(unsafe) static var shared = HTTPClient()
 
-    private init() {}
+    func get(from url: URL) {}
+}
 
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+
+    override func get(from url: URL) {
+        self.requestedURL = url
+    }
 }
 
 struct RemoteFeedLoaderTests {
     @Test func `init does not request data from URL`() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteFeedLoader()
 
         #expect(client.requestedURL == nil)
     }
 
     @Test func `load requests data from URL`() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteFeedLoader()
 
         sut.load()

0ed7c52c7e1b8bfac392e82baf074ab4ad0df96f
Ángel Vázquez <angel_betta@outlook.com>
2025-12-20 01:42:31 -0600

    Move `RemoteFeedItem` to its own file

--- Sources/EssentialFeed/Feed API/FeedItemsMapper.swift
+++ Sources/EssentialFeed/Feed API/FeedItemsMapper.swift
@@ -25,10 +25,3 @@ enum FeedItemsMapper {
         let items: [RemoteFeedItem]
     }
 }
-
-struct RemoteFeedItem: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
-}

96c89c16e99d2d7004ca458875fd60a634493e77
Ángel Vázquez <angel_betta@outlook.com>
2025-12-19 22:39:45 -0600

    Save command requests cache deletion

--- Tests/EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
+++ Tests/EssentialFeedTests/Feed Cache/CacheFeedUseCase.swift
@@ -3,10 +3,15 @@
 // Copyright © 2025 Ángel Vázquez. All rights reserved.
 //
 
+import EssentialFeed
 import XCTest
 
 final class FeedStore {
     var deleteCachedFeedCallCount = 0
+
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 final class LocalFeedLoader {
@@ -14,6 +19,10 @@ final class LocalFeedLoader {
 
     init(store: FeedStore) {
         self.store = store
+    }
+
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -23,5 +32,25 @@ final class CacheFeedUseCase: XCTestCase {
         _ = LocalFeedLoader(store: store)
 
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+
+    func test_save_requestsCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        let items = [uniqueItem()]
+
+        sut.save(items)
+
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func uniqueItem() -> FeedItem {
+        FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+    }
+
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
 }

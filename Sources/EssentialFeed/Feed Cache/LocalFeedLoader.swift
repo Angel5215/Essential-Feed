e37909f9e51a69a5e4f611e9e667d25ee0fdb204
Ángel Vázquez <angel_betta@outlook.com>
2025-12-20 18:59:34 -0600

    Move typealiases to appropriate segments

--- Sources/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ Sources/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -10,9 +10,6 @@ public final class LocalFeedLoader {
     private let currentDate: () -> Date
     private let calendar = Calendar(identifier: .gregorian)
 
-    public typealias SaveResult = Error?
-    public typealias LoadResult = LoadFeedResult
-
     public init(store: FeedStore, currentDate: @escaping () -> Date) {
         self.store = store
         self.currentDate = currentDate
@@ -30,8 +27,10 @@ public final class LocalFeedLoader {
 
 // MARK: - Save
 
-extension LocalFeedLoader {
-    public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
+public extension LocalFeedLoader {
+    typealias SaveResult = Error?
+
+    func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
         store.deleteCachedFeed { [weak self] cacheDeletionError in
             guard let self else { return }
             if let cacheDeletionError {
@@ -53,6 +52,8 @@ extension LocalFeedLoader {
 // MARK: - Load
 
 extension LocalFeedLoader: FeedLoader {
+    public typealias LoadResult = LoadFeedResult
+
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self else { return }

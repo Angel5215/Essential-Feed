fda7cf99d293b93ad0a88722247e62c0b9703b8f
Ángel Vázquez <angel_betta@outlook.com>
2025-12-20 18:29:10 -0600

    Extract cache deletion side-effect on retrieval error from the `load` method to the `validateCache` method

--- Sources/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ Sources/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -39,7 +39,6 @@ public final class LocalFeedLoader {
 
             switch result {
             case let .failure(error):
-                store.deleteCachedFeed { _ in }
                 completion(.failure(error))
 
             case let .found(feed, timestamp) where validate(timestamp):
@@ -53,6 +52,11 @@ public final class LocalFeedLoader {
                 completion(.success([]))
             }
         }
+    }
+
+    public func validateCache() {
+        store.retrieve { _ in }
+        store.deleteCachedFeed { _ in }
     }
 
     private func validate(_ timestamp: Date) -> Bool {

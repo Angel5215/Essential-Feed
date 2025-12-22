10b76af3528389ce25be97a3fefa4697e18b3e8a
Ángel Vázquez <angel_betta@outlook.com>
2025-12-21 18:05:37 -0600

    Replace production store URL with test-specific store URL to avoid sharing state/artifacts with other parts of the system (including other tests)

--- Tests/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ Tests/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -63,12 +63,12 @@ final class CodableFeedStore {
 final class CodableFeedStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
     override func tearDown() {
         super.tearDown()
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -131,14 +131,14 @@ final class CodableFeedStoreTests: XCTestCase {
     }
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
-        let sut = CodableFeedStore(storeURL: storeURL())
+        let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
 
-    private nonisolated func storeURL() -> URL {
+    private nonisolated func testSpecificStoreURL() -> URL {
         FileManager.default
-            .urls(for: .documentDirectory, in: .userDomainMask)[0]
-            .appendingPathComponent("image-feed.store")
+            .urls(for: .cachesDirectory, in: .userDomainMask)[0]
+            .appendingPathComponent("\(type(of: self)).store")
     }
 }

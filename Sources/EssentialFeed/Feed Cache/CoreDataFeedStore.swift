515cfe71ca9f58a55b502f7d0427a74b7d429451
Ángel Vázquez <angel_betta@outlook.com>
2025-12-24 13:03:22 -0600

    Add `ManagedCache` and `ManagedFeedImage` model representations

--- Sources/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ Sources/EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -3,6 +3,7 @@
 // Copyright © 2025 Ángel Vázquez. All rights reserved.
 //
 
+import CoreData
 import Foundation
 
 public final class CoreDataFeedStore: FeedStore {
@@ -15,4 +16,17 @@ public final class CoreDataFeedStore: FeedStore {
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
 
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+}
+
+private final class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private final class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }

33dac211d926f818546594cdcbd5af1c948c1241
Ángel Vázquez <angel_betta@outlook.com>
2025-12-25 01:48:30 -0600

    Separate CoreData managed model classes data from helpers with extensions

--- Sources/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
+++ Sources/EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -9,7 +9,9 @@ import CoreData
 final class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
     @NSManaged var feed: NSOrderedSet
+}
 
+extension ManagedCache {
     var localFeed: [LocalFeedImage] {
         feed.compactMap { ($0 as? ManagedFeedImage)?.local }
     }

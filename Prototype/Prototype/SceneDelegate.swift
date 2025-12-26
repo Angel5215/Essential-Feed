3118c15127a8b75e8c88098193506a9d4ebf0dc8
Ángel Vázquez <angel_betta@outlook.com>
2025-12-26 00:56:19 -0600

    Add empty project for Prototype

---
+++ Prototype/Prototype/SceneDelegate.swift
@@ -0,0 +1,10 @@
+//
+// SceneDelegate.swift
+// Copyright © 2025 Ángel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}

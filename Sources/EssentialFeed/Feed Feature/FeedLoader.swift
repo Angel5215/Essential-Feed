91be42a6d0bb770724e24e8c969f792c2411cb83
Ángel Vázquez <angel_betta@outlook.com>
2025-12-25 22:31:53 -0600

    Replace custom `LoadFeedResult` enum with standard `Swift.Result`

--- Sources/EssentialFeed/Feed Feature/FeedLoader.swift
+++ Sources/EssentialFeed/Feed Feature/FeedLoader.swift
@@ -3,10 +3,7 @@
 // Copyright © 2025 Ángel Vázquez. All rights reserved.
 //
 
-public enum LoadFeedResult {
-    case success([FeedImage])
-    case failure(Error)
-}
+public typealias LoadFeedResult = Result<[FeedImage], Error>
 
 public protocol FeedLoader {
     func load(completion: @escaping (LoadFeedResult) -> Void)

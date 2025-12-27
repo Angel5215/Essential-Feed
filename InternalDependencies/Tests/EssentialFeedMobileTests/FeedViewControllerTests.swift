48620b122b2220792ec8924d6fe211c74ab812d8
Ángel Vázquez <angel_betta@outlook.com>
2025-12-26 21:35:14 -0600

    Extract pull to refresh simulation into a reusable extension on `UIRefreshControl`

--- InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
+++ InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
@@ -8,7 +8,7 @@ import EssentialFeedMobile
 import UIKit
 import XCTest
 
-final class FeedViewController: UIViewController {
+final class FeedViewController: UITableViewController {
     private var loader: FeedLoader?
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -28,7 +28,15 @@ final class FeedViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        loader?.load { _ in }
+        refreshControl = UIRefreshControl()
+        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
+
+        load()
+    }
+
+    @objc private func load() {
+        loader?.load { _ in
+        }
     }
 }
 
@@ -48,6 +56,17 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 1)
     }
 
+    func test_pullToRefresh_loadsFeed() {
+        let (sut, loader) = makeSUT()
+        sut.loadViewIfNeeded()
+
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 2)
+
+        sut.refreshControl?.simulatePullToRefresh()
+        XCTAssertEqual(loader.loadCallCount, 3)
+    }
+
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -63,6 +82,16 @@ final class FeedViewControllerTests: XCTestCase {
 
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             loadCallCount += 1
+        }
+    }
+}
+
+private extension UIRefreshControl {
+    func simulatePullToRefresh() {
+        for target in allTargets {
+            actions(forTarget: target, forControlEvent: .valueChanged)?.forEach { action in
+                (target as NSObject).perform(Selector(action))
+            }
         }
     }
 }

6a3a657b339fdac1b8b0c8d672376bdfd60c6e12
Ángel Vázquez <angel_betta@outlook.com>
2025-12-26 21:23:56 -0600

    Loads feed on view did load

--- InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
+++ InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
@@ -4,16 +4,34 @@
 //
 
 import EssentialFeedMobile
+import UIKit
 import XCTest
 
-final class FeedViewController {
-    private let loader: FeedViewControllerTests.LoaderSpy
+final class FeedViewController: UIViewController {
+    private var loader: FeedViewControllerTests.LoaderSpy?
 
-    init(loader: FeedViewControllerTests.LoaderSpy) {
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init(nibName: nil, bundle: nil)
         self.loader = loader
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        loader?.load()
     }
 }
 
+@MainActor
 final class FeedViewControllerTests: XCTestCase {
     func test_init_doesNotLoadFeed() {
         let loader = LoaderSpy()
@@ -22,9 +40,22 @@ final class FeedViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
 
+    func test_viewDidLoad_loadsFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewController(loader: loader)
+
+        sut.loadViewIfNeeded()
+
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+
     // MARK: - Helpers
 
     final class LoaderSpy {
         private(set) var loadCallCount = 0
+
+        func load() {
+            loadCallCount += 1
+        }
     }
 }

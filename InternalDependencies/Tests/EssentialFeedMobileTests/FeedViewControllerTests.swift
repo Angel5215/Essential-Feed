d626d98977b5e7e52f2d1c7d1d521edbe8081042
Ángel Vázquez <angel_betta@outlook.com>
2025-12-26 23:30:25 -0600

    Decouple tests from specific UI loading indicators with test-specific DSL method

--- InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
+++ InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
@@ -83,7 +83,7 @@ final class FeedViewControllerTests: XCTestCase {
 
         sut.simulateAppearance()
 
-        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+        XCTAssertTrue(sut.isShowingLoadingIndicator)
     }
 
     func test_viewDidLoad_hidesLoadingIndicatorOnLoaderCompletion() {
@@ -92,7 +92,7 @@ final class FeedViewControllerTests: XCTestCase {
         sut.simulateAppearance()
         loader.completeFeedLoading()
 
-        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+        XCTAssertFalse(sut.isShowingLoadingIndicator)
     }
 
     func test_userInitiatedFeedReload_showsLoadingIndicator() {
@@ -101,7 +101,7 @@ final class FeedViewControllerTests: XCTestCase {
 
         sut.simulateUserInitiatedFeedReload()
 
-        XCTAssertEqual(sut.refreshControl?.isRefreshing, true)
+        XCTAssertTrue(sut.isShowingLoadingIndicator)
     }
 
     func test_userInitiatedFeedReload_hidesLoadingIndicatorOnLoaderCompletion() {
@@ -111,7 +111,7 @@ final class FeedViewControllerTests: XCTestCase {
         sut.simulateUserInitiatedFeedReload()
         loader.completeFeedLoading()
 
-        XCTAssertEqual(sut.refreshControl?.isRefreshing, false)
+        XCTAssertFalse(sut.isShowingLoadingIndicator)
     }
 
     // MARK: - Helpers
@@ -139,6 +139,10 @@ final class FeedViewControllerTests: XCTestCase {
 }
 
 private extension FeedViewController {
+    var isShowingLoadingIndicator: Bool {
+        refreshControl?.isRefreshing == true
+    }
+
     func simulateAppearance() {
         if !isViewLoaded {
             loadViewIfNeeded()

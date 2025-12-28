83a39738c562025cd0cab8edc3a8888f852374e9
Ángel Vázquez <angel_betta@outlook.com>
2025-12-28 01:42:06 -0600

    Move `FeedImageDataLoader` to its own file

---
+++ InternalDependencies/Sources/EssentialFeedMobile/FeedImageLoader/FeedImageDataLoader.swift
@@ -0,0 +1,15 @@
+//
+// FeedImageDataLoader.swift
+// Copyright © 2025 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}

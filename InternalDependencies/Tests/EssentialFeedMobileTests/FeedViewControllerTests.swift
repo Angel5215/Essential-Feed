093825c508d59c59a1730cb89de9878d5addc163
Ángel Vázquez <angel_betta@outlook.com>
2025-12-28 01:21:54 -0600

    Extract `UIImage` test helper to a separate file

--- InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
+++ InternalDependencies/Tests/EssentialFeedMobileTests/FeedViewControllerTests.swift
@@ -516,16 +516,3 @@ private extension UIButton {
         }
     }
 }
-
-extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let size = CGSize(width: 1, height: 1)
-        let rect = CGRect(origin: .zero, size: size)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-        return UIGraphicsImageRenderer(size: size, format: format).image { context in
-            color.setFill()
-            context.fill(rect)
-        }
-    }
-}

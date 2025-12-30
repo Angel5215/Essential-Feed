4faab436d33b35e2bfd3d3c65e5c40ab8edd168f
Ángel Vázquez <angel_betta@outlook.com>
2025-12-29 22:09:20 -0600

    Move main thread dispatch to the presenter

--- InternalDependencies/Sources/EssentialFeedMobile/FeedUI/Controllers/FeedViewController.swift
+++ InternalDependencies/Sources/EssentialFeedMobile/FeedUI/Controllers/FeedViewController.swift
@@ -16,13 +16,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
     var tableModel = [FeedImageCellController]() {
         didSet {
-            if Thread.isMainThread {
-                tableView.reloadData()
-            } else {
-                DispatchQueue.main.async { [weak self] in
-                    self?.tableView.reloadData()
-                }
-            }
+            tableView.reloadData()
         }
     }
 
@@ -78,11 +72,6 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
 
 extension FeedViewController: FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel) {
-        guard Thread.isMainThread else {
-            return DispatchQueue.main.async { [weak self] in
-                self?.display(viewModel)
-            }
-        }
         if viewModel.isLoading {
             refreshControl?.beginRefreshing()
         } else {

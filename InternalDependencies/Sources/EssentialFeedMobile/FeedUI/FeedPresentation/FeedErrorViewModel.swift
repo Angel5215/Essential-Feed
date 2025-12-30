eb685fa91955579281b6a3d8345f3d5a90633bcd
Ángel Vázquez <angel_betta@outlook.com>
2025-12-29 23:24:00 -0600

    Add `FeedErrorViewModel` factory helpers to clarify intent in the point of use

--- InternalDependencies/Sources/EssentialFeedMobile/FeedUI/FeedPresentation/FeedErrorViewModel.swift
+++ InternalDependencies/Sources/EssentialFeedMobile/FeedUI/FeedPresentation/FeedErrorViewModel.swift
@@ -5,4 +5,12 @@
 
 struct FeedErrorViewModel {
     let message: String?
+
+    static var noError: FeedErrorViewModel {
+        FeedErrorViewModel(message: nil)
+    }
+
+    static func error(message: String) -> FeedErrorViewModel {
+        FeedErrorViewModel(message: message)
+    }
 }
--- InternalDependencies/Sources/EssentialFeedMobile/FeedUI/FeedPresentation/FeedPresenter.swift
+++ InternalDependencies/Sources/EssentialFeedMobile/FeedUI/FeedPresentation/FeedPresenter.swift
@@ -27,7 +27,7 @@ final class FeedPresenter {
 
     func didStartLoadingFeed() {
         loadingView.display(FeedLoadingViewModel(isLoading: true))
-        errorView.display(FeedErrorViewModel(message: nil))
+        errorView.display(.noError)
     }
 
     func didFinishLoadingFeed(with feed: [FeedImage]) {
@@ -37,7 +37,7 @@ final class FeedPresenter {
 
     func didFinishLoadingFeed(with error: Error) {
         loadingView.display(FeedLoadingViewModel(isLoading: false))
-        errorView.display(FeedErrorViewModel(message: feedLoadError))
+        errorView.display(.error(message: feedLoadError))
     }
 }
 

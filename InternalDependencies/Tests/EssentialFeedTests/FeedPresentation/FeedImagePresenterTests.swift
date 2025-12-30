cc5dbba4ddc17e5a371c745d911596faf064fae1
Ángel Vázquez <angel_betta@outlook.com>
2025-12-30 14:12:40 -0600

    Inline `Data` creation

--- InternalDependencies/Tests/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
+++ InternalDependencies/Tests/EssentialFeedTests/FeedPresentation/FeedImagePresenterTests.swift
@@ -95,9 +95,8 @@ final class FeedImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
         let (sut, view) = makeSUT(imageTransformer: fail)
         let image = uniqueImage()
-        let data = Data()
 
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with: Data(), for: image)
 
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)

4b4a78b62f31ef1286be3bfdf247283e7815e3a3
Ángel Vázquez <angel_betta@outlook.com>
2025-12-31 01:16:18 -0600

    Remove default `URLSessionHTTPClient` initializer parameter
    Clients should be mindful about which `URLSession` instance to use. For example,
    clients may decide to use the `.shared` instance, but when testing we can inject
    a `URLSession` with an ephemeral configuration registered with the `URLProtocolStub`
    class. This way, we don't need to start/stop intercepting URL requests globally.

--- InternalDependencies/Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ InternalDependencies/Tests/EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -8,12 +8,8 @@ import Foundation
 import XCTest
 
 final class URLSessionHTTPClientTests: XCTestCase {
-    override func setUp() {
-        URLProtocolStub.startInterceptingRequests()
-    }
-
     override func tearDown() {
-        URLProtocolStub.stopInterceptingRequests()
+        URLProtocolStub.removeStub()
     }
 
     func test_getFromURL_performsGETRequestWithURL() {
@@ -83,7 +79,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> any HTTPClient {
-        let sut = URLSessionHTTPClient()
+        let configuration = URLSessionConfiguration.ephemeral
+        configuration.protocolClasses = [URLProtocolStub.self]
+        let session = URLSession(configuration: configuration)
+        let sut = URLSessionHTTPClient(session: session)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
@@ -201,12 +200,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
         }
 
-        static func startInterceptingRequests() {
-            URLProtocol.registerClass(URLProtocolStub.self)
-        }
-
-        static func stopInterceptingRequests() {
-            URLProtocol.unregisterClass(URLProtocolStub.self)
+        static func removeStub() {
             stub = nil
         }
 

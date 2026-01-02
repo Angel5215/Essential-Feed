310759ae212ec55434642979df45c0151d9c3c6a
Ángel Vázquez <angel_betta@outlook.com>
2026-01-01 21:54:04 -0600

    Move `LocalFeedImageDataLoader` and `FeedImageDataStore` to production

---
+++ InternalDependencies/Sources/EssentialFeed/FeedCache/FeedImageDataStore.swift
@@ -0,0 +1,11 @@
+//
+// FeedImageDataStore.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataStore {
+    typealias Result = Swift.Result<Data?, Error>
+    func retrieve(dataForURL url: URL, completion: @escaping (Result) -> Void)
+}

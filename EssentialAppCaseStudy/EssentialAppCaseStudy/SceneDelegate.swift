e381b6b5f0b5d0324cca90d073127c64713d875f
Ángel Vázquez <angel_betta@outlook.com>
2026-01-03 14:45:56 -0600

    Use loader composites in main composition

--- EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
@@ -3,6 +3,7 @@
 // Copyright © 2026 Ángel Vázquez. All rights reserved.
 //
 
+import CoreData
 import EssentialFeed
 import EssentialFeedMobile
 import UIKit
@@ -12,5 +13,23 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let _ = (scene as? UIWindowScene) else { return }
+
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        let remoteClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        let localStoreURL = NSPersistentContainer.defaultDirectoryURL().appending(path: "feed-store.sqlite")
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = FeedUIComposer.feedComposedWith(
+            feedLoader: FeedLoaderWithFallbackComposite(
+                primary: RemoteFeedLoader(url: remoteURL, client: remoteClient),
+                fallback: LocalFeedLoader(store: localStore, currentDate: Date.init),
+            ),
+            imageLoader: FeedImageDataLoaderWithFallbackComposite(
+                primary: LocalFeedImageDataLoader(store: localStore),
+                fallback: RemoteFeedImageDataLoader(client: remoteClient),
+            ),
+        )
+        window?.makeKeyAndVisible()
     }
 }

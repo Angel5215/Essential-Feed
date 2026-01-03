b422c93b621661d7449017edc15d8397312da5ba
Ángel Vázquez <angel_betta@outlook.com>
2026-01-02 22:37:39 -0600

    Add empty `EssentialAppCaseStudy` project

---
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/ViewController.swift
@@ -0,0 +1,13 @@
+//
+// ViewController.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ EssentialAppCaseStudy/EssentialAppCaseStudyUITests/EssentialAppCaseStudyUITestsLaunchTests.swift
@@ -0,0 +1,30 @@
+//
+// EssentialAppCaseStudyUITestsLaunchTests.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import XCTest
+
+final class EssentialAppCaseStudyUITestsLaunchTests: XCTestCase {
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

eed7b558a7438fe3fca62c997957c7219deeca2b
Ángel Vázquez <angel_betta@outlook.com>
2026-01-03 12:06:45 -0600

    `FeedImageDataLoaderWithFallbackComposite.loadImageData` loads from primary loader first

--- EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -16,7 +16,8 @@ final class FeedImageDataLoaderWithFallbackComposite: FeedImageDataLoader {
     }
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        Task()
+        _ = primary.loadImageData(from: url, completion: completion)
+        return Task()
     }
 
     private final class Task: FeedImageDataLoaderTask {
@@ -31,6 +32,18 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         _ = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
 
         XCTAssertTrue(primaryLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the primary loader")
+        XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
+    }
+
+    func test_loadImageData_loadsFromPrimaryLoaderFirst() {
+        let url = anyURL()
+        let primaryLoader = LoaderSpy()
+        let fallbackLoader = LoaderSpy()
+        let sut = FeedImageDataLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+
+        _ = sut.loadImageData(from: url) { _ in }
+
+        XCTAssertEqual(primaryLoader.loadedURLs, [url], "Expected to load URL from primary loader")
         XCTAssertTrue(fallbackLoader.loadedURLs.isEmpty, "Expected no loaded URLs in the fallback loader")
     }
 
@@ -51,5 +64,9 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         private final class Task: FeedImageDataLoaderTask {
             func cancel() {}
         }
+    }
+
+    private func anyURL() -> URL {
+        URL(string: "https://any-url.com")!
     }
 }

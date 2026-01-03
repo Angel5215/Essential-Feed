c41fb2d0fd57cb1e6c1168c438dd53612a80f1cd
Ángel Vázquez <angel_betta@outlook.com>
2026-01-02 23:25:49 -0600

    `FeedLoaderWithFallbackComposite.load` delivers primary feed on primary loader success

---
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,63 @@
+//
+// FeedLoaderWithFallbackCompositeTests.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import EssentialFeed
+import XCTest
+
+final class FeedLoaderWithFallbackComposite: FeedLoader {
+    private let primary: FeedLoader
+    private let fallback: FeedLoader
+
+    init(primary: FeedLoader, fallback: FeedLoader) {
+        self.primary = primary
+        self.fallback = fallback
+    }
+
+    func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        primary.load(completion: completion)
+    }
+}
+
+final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
+        let primaryFeed = uniqueFeed()
+        let fallbackFeed = uniqueFeed()
+        let primaryLoader = LoaderStub(result: .success(primaryFeed))
+        let fallbackLoader = LoaderStub(result: .success(fallbackFeed))
+        let sut = FeedLoaderWithFallbackComposite(primary: primaryLoader, fallback: fallbackLoader)
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, primaryFeed)
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1)
+    }
+
+    // MARK: - Helpers
+
+    private func uniqueFeed() -> [FeedImage] {
+        [
+            FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "https://any-url.com")!)
+        ]
+    }
+
+    private final class LoaderStub: FeedLoader {
+        private let result: FeedLoader.Result
+
+        init(result: FeedLoader.Result) {
+            self.result = result
+        }
+
+        func load(completion: @escaping (FeedLoader.Result) -> Void) {
+            completion(result)
+        }
+    }
+}

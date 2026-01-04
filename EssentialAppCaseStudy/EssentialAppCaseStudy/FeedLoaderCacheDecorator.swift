abc0631dcdd52b06528b9aa03a869f985f1a731b
Ángel Vázquez <angel_betta@outlook.com>
2026-01-04 00:55:11 -0600

    Move `FeedLoaderCacheDecorator` to production

---
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,25 @@
+//
+// FeedLoaderCacheDecorator.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import EssentialFeed
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
--- EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedLoaderCacheDecoratorTests.swift
@@ -3,27 +3,9 @@
 // Copyright © 2026 Ángel Vázquez. All rights reserved.
 //
 
+import EssentialAppCaseStudy
 import EssentialFeed
 import XCTest
-
-final class FeedLoaderCacheDecorator: FeedLoader {
-    private let decoratee: FeedLoader
-    private let cache: FeedCache
-
-    init(decoratee: FeedLoader, cache: FeedCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-
-    func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        decoratee.load { [weak self] result in
-            completion(result.map { feed in
-                self?.cache.save(feed) { _ in }
-                return feed
-            })
-        }
-    }
-}
 
 final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     func test_load_deliversFeedOnLoaderSuccess() {

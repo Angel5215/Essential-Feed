6956502364fbf2436956b64068560b6cd02c7ad5
Ángel Vázquez <angel_betta@outlook.com>
2026-01-03 22:59:05 -0600

    Extract `FeedLoader` test helpers into a shared protocol extension to remove duplication

--- EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -7,7 +7,7 @@ import EssentialAppCaseStudy
 import EssentialFeed
 import XCTest
 
-final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
+final class FeedLoaderWithFallbackCompositeTests: XCTestCase, FeedLoaderTestCase {
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
         let primaryFeed = uniqueFeed()
         let fallbackFeed = uniqueFeed()
@@ -39,22 +39,5 @@ final class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         trackForMemoryLeaks(fallbackLoader, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
-    }
-
-    private func expect(_ sut: FeedLoader, toCompleteWith expectedResult: FeedLoader.Result, file: StaticString = #filePath, line: UInt = #line) {
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { receivedResult in
-            switch (receivedResult, expectedResult) {
-            case let (.success(receivedFeed), .success(expectedFeed)):
-                XCTAssertEqual(receivedFeed, expectedFeed, file: file, line: line)
-            case (.failure, .failure):
-                break
-            default:
-                XCTFail("Expected \(expectedResult), got \(receivedResult) instead", file: file, line: line)
-            }
-            exp.fulfill()
-        }
-
-        wait(for: [exp], timeout: 1)
     }
 }

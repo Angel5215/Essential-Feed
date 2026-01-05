89c412379f8304802b7e448d6a333f146924f86e
Ángel Vázquez <angel_betta@outlook.com>
2026-01-05 01:07:58 -0600

    Displays empty feed when customer has no connectivity and no cache

--- EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
@@ -20,6 +20,11 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
 
         let localStoreURL = NSPersistentContainer.defaultDirectoryURL.appending(path: "feed-store.sqlite")
+
+        if CommandLine.arguments.contains("-reset") {
+            try? FileManager.default.removeItem(at: localStoreURL)
+        }
+
         let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
         let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: localStore)

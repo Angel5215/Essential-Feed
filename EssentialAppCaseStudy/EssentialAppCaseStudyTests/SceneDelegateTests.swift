5c4cbe8c0718dde36c849c41bfe034b2d23653c3
Ángel Vázquez <angel_betta@outlook.com>
2026-01-07 23:12:23 -0600

    Solve warning for iOS 26+. `UIWindow.init()` is deprecated, now requires a `UIWindowScene`

--- EssentialAppCaseStudy/EssentialAppCaseStudyTests/SceneDelegateTests.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/SceneDelegateTests.swift
@@ -8,9 +8,9 @@ import EssentialFeedMobile
 import XCTest
 
 final class SceneDelegateTests: XCTestCase {
-    func test_sceneWillConnectToSession_configuresRootViewController() {
+    func test_sceneWillConnectToSession_configuresRootViewController() throws {
         let sut = SceneDelegate()
-        sut.window = UIWindow()
+        sut.window = try UIWindowSpy.make()
 
         sut.configureWindow()
 
@@ -20,5 +20,16 @@ final class SceneDelegateTests: XCTestCase {
 
         XCTAssertNotNil(rootNavigation, "Expected a navigation controller as root, got \(String(describing: root)) instead.")
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
+    }
+
+    // MARK: - Helpers
+
+    private final class UIWindowSpy: UIWindow {
+        static func make(file: StaticString = #filePath, line: UInt = #line) throws -> UIWindowSpy {
+            let dummyScene = try XCTUnwrap((UIWindowScene.self as NSObject.Type).init() as? UIWindowScene)
+            return UIWindowSpy(windowScene: dummyScene)
+        }
+
+        override func makeKeyAndVisible() {}
     }
 }

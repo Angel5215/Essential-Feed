1b6bd1fd2a5f6d46275f953aa410aa5d997d599d
Ángel Vázquez <angel_betta@outlook.com>
2026-01-08 14:59:07 -0600

    Remove unused debugging code that was previously used during UI tests

--- EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
@@ -12,11 +12,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    let localStoreURL = NSPersistentContainer.defaultDirectoryURL.appending(path: "feed-store.sqlite")
-
-    private lazy var httpClient: HTTPClient = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
-
-    private lazy var store: FeedStore & FeedImageDataStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+    private lazy var httpClient = makeRemoteClient()
+    private lazy var store = makeLocalStore()
 
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
         self.init()
@@ -32,9 +29,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func configureWindow() {
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        let remoteClient = makeRemoteClient()
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: remoteClient)
-        let remoteImageLoader = RemoteFeedImageDataLoader(client: remoteClient)
+        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
+        let remoteImageLoader = RemoteFeedImageDataLoader(client: httpClient)
 
         let localFeedLoader = LocalFeedLoader(store: store, currentDate: Date.init)
         let localImageLoader = LocalFeedImageDataLoader(store: store)
@@ -61,7 +57,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
     }
 
-    func makeRemoteClient() -> HTTPClient {
-        httpClient
+    private func makeRemoteClient() -> HTTPClient {
+        URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+    }
+
+    private func makeLocalStore() -> FeedStore & FeedImageDataStore {
+        try! CoreDataFeedStore(
+            storeURL: NSPersistentContainer.defaultDirectoryURL.appending(path: "feed-store.sqlite")
+        )
     }
 }

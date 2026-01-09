a2234e4475267e218efc80b703062fabe42f83a7
Ángel Vázquez <angel_betta@outlook.com>
2026-01-09 00:55:15 -0600

    Remove unnecessary `Main.storyboard` - the initial setup happens in code

--- EssentialAppCaseStudy/EssentialAppCaseStudyTests/SceneDelegateTests.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudyTests/SceneDelegateTests.swift
@@ -22,14 +22,28 @@ final class SceneDelegateTests: XCTestCase {
         XCTAssertTrue(topController is FeedViewController, "Expected a feed controller as top view controller, got \(String(describing: topController)) instead")
     }
 
+    func test_configureWindow_setsUpWindowAsKeyAndVisible() throws {
+        let window = try UIWindowSpy.make()
+        let sut = SceneDelegate()
+        sut.window = window
+
+        sut.configureWindow()
+
+        XCTAssertEqual(window.makeKeyAndVisibleCallCount, 1)
+    }
+
     // MARK: - Helpers
 
     private final class UIWindowSpy: UIWindow {
+        private(set) var makeKeyAndVisibleCallCount = 0
+
         static func make(file: StaticString = #filePath, line: UInt = #line) throws -> UIWindowSpy {
             let dummyScene = try XCTUnwrap((UIWindowScene.self as NSObject.Type).init() as? UIWindowScene)
             return UIWindowSpy(windowScene: dummyScene)
         }
 
-        override func makeKeyAndVisible() {}
+        override func makeKeyAndVisible() {
+            makeKeyAndVisibleCallCount += 1
+        }
     }
 }

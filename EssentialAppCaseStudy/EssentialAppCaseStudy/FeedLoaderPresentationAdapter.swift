cab17bf5a0cc2f0453e05e2d2f59198d8d1f0bd6
Ángel Vázquez <angel_betta@outlook.com>
2026-01-11 23:12:26 -0600

    Move `dispatchOnMainQueue` operator to the point of use (before subscription with `sink`)

--- EssentialAppCaseStudy/EssentialAppCaseStudy/FeedLoaderPresentationAdapter.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/FeedLoaderPresentationAdapter.swift
@@ -6,28 +6,31 @@
 import Combine
 import EssentialFeed
 import EssentialFeedMobile
+import Foundation
 
 final class FeedLoaderPresentationAdapter: FeedViewControllerDelegate {
-    private let feedLoader: FeedLoader.Publisher
+    private let feedLoader: () -> FeedLoader.Publisher
     private var cancellable: Cancellable?
     var presenter: FeedPresenter?
 
-    init(feedLoader: FeedLoader.Publisher) {
+    init(feedLoader: @escaping () -> FeedLoader.Publisher) {
         self.feedLoader = feedLoader
     }
 
     func didRequestFeedRefresh() {
         presenter?.didStartLoadingFeed()
 
-        cancellable = feedLoader.sink { [weak self] completion in
-            switch completion {
-            case .finished:
-                break
-            case let .failure(error):
-                self?.presenter?.didFinishLoadingFeed(with: error)
+        cancellable = feedLoader()
+            .dispatchOnMainQueue()
+            .sink { [weak self] completion in
+                switch completion {
+                case .finished:
+                    break
+                case let .failure(error):
+                    self?.presenter?.didFinishLoadingFeed(with: error)
+                }
+            } receiveValue: { [weak self] feed in
+                self?.presenter?.didFinishLoadingFeed(with: feed)
             }
-        } receiveValue: { [weak self] feed in
-            self?.presenter?.didFinishLoadingFeed(with: feed)
-        }
     }
 }
--- EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/SceneDelegate.swift
@@ -14,6 +14,7 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     private lazy var httpClient = makeRemoteClient()
     private lazy var store = makeLocalStore()
     private lazy var localFeedLoader = makeLocalFeedLoader()
+    private lazy var remoteFeedLoader = makeRemoteFeedLoader()
 
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
         self.init()
@@ -55,15 +56,17 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         )
     }
 
+    private func makeRemoteFeedLoader() -> RemoteFeedLoader {
+        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
+        return RemoteFeedLoader(url: remoteURL, client: httpClient)
+    }
+
     private func makeLocalFeedLoader() -> LocalFeedLoader {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }
 
     private func makeRemoteFeedLoaderWithLocalFallback() -> FeedLoader.Publisher {
-        let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: httpClient)
-
-        return remoteFeedLoader
+        remoteFeedLoader
             .loadPublisher()
             .caching(to: localFeedLoader)
             .fallback(to: localFeedLoader.loadPublisher)

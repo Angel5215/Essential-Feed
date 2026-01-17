cbf8b1346ae5934a4ccbc06663208436b9fff7aa
Ángel Vázquez <angel_betta@outlook.com>
2026-01-17 00:03:58 -0600

    Duplicate `RemoteFeedLoader` as `RemoteImageCommentsLoader`

---
+++ InternalDependencies/Sources/EssentialFeed/FeedAPI/ImageCommentsMapper.swift
@@ -0,0 +1,22 @@
+//
+// ImageCommentsMapper.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+enum ImageCommentsMapper {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws(RemoteImageCommentsLoader.Error) -> [RemoteFeedItem] {
+        guard response.isOK, let root = try? JSONDecoder().decode(Root.self, from: data) else {
+            throw .invalidData
+        }
+
+        return root.items
+    }
+
+    // MARK: - Helpers
+
+    private struct Root: Decodable {
+        let items: [RemoteFeedItem]
+    }
+}

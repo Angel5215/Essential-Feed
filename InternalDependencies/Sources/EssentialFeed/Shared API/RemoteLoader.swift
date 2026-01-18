3377d52c1e1e47aef9d799e917196be48981954c
Ángel Vázquez <angel_betta@outlook.com>
2026-01-17 20:35:30 -0600

    Duplicate `RemoteFeedLoader` as `RemoteLoader`

---
+++ InternalDependencies/Sources/EssentialFeed/Shared API/RemoteLoader.swift
@@ -0,0 +1,44 @@
+//
+// RemoteLoader.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+import Foundation
+
+public final class RemoteLoader: FeedLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public typealias Result = FeedLoader.Result
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(Self.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}

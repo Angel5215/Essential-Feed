e999319abca4c35f32a6b72548240a9149f75fb5
Ángel Vázquez <angel_betta@outlook.com>
2026-01-27 13:15:17 -0600

    Replace `FeedPresenter` with generic presenter

--- EssentialAppCaseStudy/EssentialAppCaseStudy/FeedUIComposer.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/FeedUIComposer.swift
@@ -15,10 +15,11 @@ public enum FeedUIComposer {
     ) -> FeedViewController {
         let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: feedLoader)
         let feedController = makeFeedViewController(delegate: presentationAdapter, title: FeedPresenter.title)
-        presentationAdapter.presenter = FeedPresenter(
-            feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+        presentationAdapter.presenter = LoadResourcePresenter<[FeedImage], FeedViewAdapter>(
+            resourceView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
             loadingView: WeakReferenceVirtualProxy(feedController),
             errorView: WeakReferenceVirtualProxy(feedController),
+            mapper: FeedPresenter.map,
         )
         return feedController
     }
--- EssentialAppCaseStudy/EssentialAppCaseStudy/FeedViewAdapter.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/FeedViewAdapter.swift
@@ -7,7 +7,7 @@ import EssentialFeed
 import EssentialFeedMobile
 import UIKit
 
-final class FeedViewAdapter: FeedView {
+final class FeedViewAdapter: ResourceView {
     private weak var controller: FeedViewController?
     private let imageLoader: (URL) -> FeedImageDataLoader.Publisher
 

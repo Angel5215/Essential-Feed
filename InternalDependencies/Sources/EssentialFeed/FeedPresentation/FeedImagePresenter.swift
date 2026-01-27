4196430c84e8559a71c382bfe353b0aaa7ad6398
Ángel Vázquez <angel_betta@outlook.com>
2026-01-27 13:44:35 -0600

    Add `FeedImagePresenter.map`

--- InternalDependencies/Sources/EssentialFeed/FeedPresentation/FeedImagePresenter.swift
+++ InternalDependencies/Sources/EssentialFeed/FeedPresentation/FeedImagePresenter.swift
@@ -50,6 +50,16 @@ public final class FeedImagePresenter<View: FeedImageView, Image> where View.Ima
             )
         )
     }
+
+    public static func map(_ image: FeedImage) -> FeedImageViewModel<Image> {
+        FeedImageViewModel(
+            description: image.description,
+            location: image.location,
+            image: nil,
+            isLoading: false,
+            shouldRetry: false,
+        )
+    }
 }
 
 // MARK: - Views

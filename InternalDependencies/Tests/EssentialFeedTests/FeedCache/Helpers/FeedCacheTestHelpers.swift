9d7f1d9de959f690a1fbced8b4719ac62a98ad64
Ángel Vázquez <angel_betta@outlook.com>
2026-03-02 01:45:46 -0600

    Map `ImageComment`s into `ImageCommentsViewModel`s

--- InternalDependencies/Tests/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ InternalDependencies/Tests/EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -26,16 +26,4 @@ extension Date {
     func minusFeedCacheMaxAge() -> Date {
         adding(days: -feedCacheMaxAgeInDays)
     }
-
-    private func adding(days: Int) -> Date {
-        Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
-    }
-}
-
-// MARK: - Date Helpers
-
-extension Date {
-    func adding(seconds: TimeInterval) -> Date {
-        self + seconds
-    }
 }

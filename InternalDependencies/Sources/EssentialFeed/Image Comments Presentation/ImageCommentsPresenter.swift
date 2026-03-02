86bf6d12bf61e3e41abfa5e8bcc184ed144c082a
Ángel Vázquez <angel_betta@outlook.com>
2026-03-02 01:24:06 -0600

    Add image comments title

---
+++ InternalDependencies/Sources/EssentialFeed/Image Comments Presentation/ImageCommentsPresenter.swift
@@ -0,0 +1,10 @@
+//
+// ImageCommentsPresenter.swift
+// Copyright © 2026 Ángel Vázquez. All rights reserved.
+//
+
+public enum ImageCommentsPresenter {
+    public static var title: String {
+        String(localized: "IMAGE_COMMENTS_VIEW_TITLE", table: "ImageComments", bundle: .module, comment: "Title for the feed view")
+    }
+}

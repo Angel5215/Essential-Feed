4ae658b6a8cc7f00901bff45e02a72fd092170ce
Ángel Vázquez <angel_betta@outlook.com>
2026-03-27 15:47:21 -0600

    Make `FeedImageDataCache` synchronous

--- InternalDependencies/Sources/EssentialFeed/Feed/Cache/LocalFeedImageDataLoader.swift
+++ InternalDependencies/Sources/EssentialFeed/Feed/Cache/LocalFeedImageDataLoader.swift
@@ -16,15 +16,12 @@ public final class LocalFeedImageDataLoader {
 // MARK: - Save
 
 extension LocalFeedImageDataLoader: FeedImageDataCache {
-    public typealias SaveResult = FeedImageDataCache.Result
-
-    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-        completion(
-            Result {
-                try store.insert(data, for: url)
-            }
-            .mapError { _ in SaveError.failed }
-        )
+    public func save(_ data: Data, for url: URL) throws {
+        do {
+            try store.insert(data, for: url)
+        } catch {
+            throw SaveError.failed
+        }
     }
 
     public enum SaveError: Error {

18d997ad2814ee8b5274f7ea53ebe5b48533fa95
Ángel Vázquez <angel_betta@outlook.com>
2026-03-27 22:14:42 -0600

    Make `NullStore` implementation for `FeedImageDataStore` synchronous

--- EssentialAppCaseStudy/EssentialAppCaseStudy/CompositionRoot/Helpers/NullStore.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/CompositionRoot/Helpers/NullStore.swift
@@ -23,11 +23,9 @@ extension NullStore: FeedStore {
 }
 
 extension NullStore: FeedImageDataStore {
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(nil))
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        nil
     }
 
-    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
-        completion(.success(()))
-    }
+    func insert(_ data: Data, for url: URL) throws {}
 }

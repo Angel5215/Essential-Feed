39f3864860ba7c714a52ff5c0b0a975c7b703cd4
Ángel Vázquez <angel_betta@outlook.com>
2026-03-27 22:16:41 -0600

    Remove unused async methods

--- InternalDependencies/Tests/EssentialFeedTests/Feed/Cache/CoreDataFeedImageDataStoreTests.swift
+++ InternalDependencies/Tests/EssentialFeedTests/Feed/Cache/CoreDataFeedImageDataStoreTests.swift
@@ -45,28 +45,6 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: url)
     }
 
-    func test_sideEffects_runSerially() {
-        let sut = makeSUT()
-        let url = anyURL()
-
-        let op1 = expectation(description: "Operation 1")
-        sut.insert([localImage(url: url)], timestamp: Date()) { _ in
-            op1.fulfill()
-        }
-
-        let op2 = expectation(description: "Operation 2")
-        sut.insert(anyData(), for: url) { _ in
-            op2.fulfill()
-        }
-
-        let op3 = expectation(description: "Operation 3")
-        sut.insert(anyData(), for: url) { _ in
-            op3.fulfill()
-        }
-
-        wait(for: [op1, op2, op3], timeout: 5, enforceOrder: true)
-    }
-
     // MARK: - Helpers
 
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CoreDataFeedStore {
@@ -76,17 +54,17 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         return sut
     }
 
-    private func notFound() -> FeedImageDataStore.RetrievalResult {
+    private func notFound() -> Result<Data?, Error> {
         .success(nil)
     }
 
-    private func found(_ data: Data) -> FeedImageDataStore.RetrievalResult {
+    private func found(_ data: Data) -> Result<Data?, Error> {
         .success(data)
     }
 
     private func expect(
         _ sut: CoreDataFeedStore,
-        toCompleteRetrievalWith expectedResult: FeedImageDataStore.RetrievalResult,
+        toCompleteRetrievalWith expectedResult: Result<Data?, Error>,
         for url: URL,
         file: StaticString = #file,
         line: UInt = #line,

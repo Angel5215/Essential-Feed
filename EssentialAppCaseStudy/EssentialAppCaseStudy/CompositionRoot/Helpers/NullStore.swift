d24ef8886e4a668bac14f29d60f50d616faef116
Ángel Vázquez <angel_betta@outlook.com>
2026-03-29 02:26:24 -0600

    Make `NullStore` implementation synchronous

--- EssentialAppCaseStudy/EssentialAppCaseStudy/CompositionRoot/Helpers/NullStore.swift
+++ EssentialAppCaseStudy/EssentialAppCaseStudy/CompositionRoot/Helpers/NullStore.swift
@@ -9,16 +9,12 @@ import Foundation
 final class NullStore {}
 
 extension NullStore: FeedStore {
-    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(.success(()))
-    }
+    func deleteCachedFeed() throws {}
 
-    func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        completion(.success(()))
-    }
+    func insert(_ feed: [LocalFeedImage], timestamp: Date) throws {}
 
-    func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.success(nil))
+    func retrieve() throws -> CachedFeed? {
+        nil
     }
 }
 

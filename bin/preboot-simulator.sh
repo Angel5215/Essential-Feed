37adaf332dece2221dcc205f453bd13b348ab4b5
Ángel Vázquez <angel_betta@outlook.com>
2026-03-30 16:46:55 -0600

    Verify simulator is loaded successfully

--- bin/preboot-simulator.sh
+++ bin/preboot-simulator.sh
@@ -4,3 +4,10 @@ set -o pipefail
 
 xcrun simctl boot "$SIMULATOR_UDID"
 xcrun simctl list devices | grep "Booted"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to boot the simulator with UDID: $SIMULATOR_UDID"
+    exit 1
+else
+    echo "Simulator with UDID: $SIMULATOR_UDID is booted successfully."
+fi

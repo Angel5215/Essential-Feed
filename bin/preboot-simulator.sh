086b2a4c69f6212c1cf293002c5e05721e14ab21
Ángel Vázquez <angel_betta@outlook.com>
2026-04-03 01:52:06 -0600

    Fix environment resolution for simulator UDID

--- bin/preboot-simulator.sh
+++ bin/preboot-simulator.sh
@@ -2,18 +2,24 @@
 
 set -o pipefail
 
-SIMULATOR_UDID=$(xcrun simctl list devices --json | jq -r '
-.devices["com.apple.CoreSimulator.SimRuntime.iOS-$SIMULATOR_RUNTIME"][] 
-| select(.name == "$SIMULATOR_NAME") 
-| .udid
-')
+SIMULATOR_UDID=$(xcrun simctl list devices --json | jq -r \
+  --arg runtime "$SIMULATOR_RUNTIME" \
+  --arg name "$SIMULATOR_NAME" \
+  '.devices["com.apple.CoreSimulator.SimRuntime.iOS-\($runtime)"][] | select(.name == $name) | .udid'
+)
+
+if [ -z "$SIMULATOR_UDID" ]; then
+    echo "ERROR: Could not find simulator $SIMULATOR_NAME for runtime iOS-$SIMULATOR_RUNTIME"
+    xcrun simctl list devices available
+    exit 1
+fi
 
 echo "Attempting to boot the simulator with UDID: $SIMULATOR_UDID ($SIMULATOR_NAME - $SIMULATOR_RUNTIME)"
 xcrun simctl boot "$SIMULATOR_UDID"
 
 if [ $? -ne 0 ]; then
     echo "Failed to boot the simulator with UDID: $SIMULATOR_UDID"
-    exit 1
+    exit 2
 else
     echo "Simulator with UDID: $SIMULATOR_UDID is booted successfully."
     xcrun simctl list devices | grep "Booted"

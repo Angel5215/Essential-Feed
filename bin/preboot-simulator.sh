a418aa0e734f42ef5815c7d53182d9f7c1f43366
Ángel Vázquez <angel_betta@outlook.com>
2026-04-03 00:13:38 -0600

    Preboot simulator using name and runtime instead of relying in hardcoded UDID

--- bin/preboot-simulator.sh
+++ bin/preboot-simulator.sh
@@ -2,15 +2,19 @@
 
 set -o pipefail
 
-echo "Available devices"
-xcrun simctl list devices
+SIMULATOR_UDID=$(xcrun simctl list devices --json | jq -r '
+.devices["com.apple.CoreSimulator.SimRuntime.iOS-26-4"][] 
+| select(.name == "iPhone 17") 
+| .udid
+')
 
+echo "Attempting to boot the simulator with UDID: $SIMULATOR_UDID and name: $SIMULATOR_NAME for runtime: $SIMULATOR_RUNTIME"
 xcrun simctl boot "$SIMULATOR_UDID"
-xcrun simctl list devices | grep "Booted"
 
 if [ $? -ne 0 ]; then
     echo "Failed to boot the simulator with UDID: $SIMULATOR_UDID"
     exit 1
 else
     echo "Simulator with UDID: $SIMULATOR_UDID is booted successfully."
+    xcrun simctl list devices | grep "Booted"
 fi

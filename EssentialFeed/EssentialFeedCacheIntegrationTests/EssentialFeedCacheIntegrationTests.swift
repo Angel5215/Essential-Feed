d3f9a79a22538ba514f4da43ba52efc3a1cf8a1d
Ángel Vázquez <angel_betta@outlook.com>
2026-05-28 12:28:52 -0600

    Fix warnings for `CoreDataFeedStore` and related tests

--- EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeed/EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -8,14 +8,14 @@ import XCTest
 
 @MainActor
 final class EssentialFeedCacheIntegrationTests: XCTestCase {
-    override func setUp() {
-        super.setUp()
+    override func setUp() async throws {
+        try await super.setUp()
 
         setupEmptyStoreState()
     }
 
-    override func tearDown() {
-        super.tearDown()
+    override func tearDown() async throws {
+        try await super.tearDown()
 
         undoStoreSideEffects()
     }

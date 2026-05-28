d166e6222f2704806c356fe223db030e6651ed43
Ángel Vázquez <angel_betta@outlook.com>
2026-05-28 12:59:33 -0600

    Enable MainActor isolation in `EssentialFeedMobile` and fix warnings

--- EssentialFeed/EssentialFeedMobile/Feed/UI/Views/Helpers/UIView+Shimmering.swift
+++ EssentialFeed/EssentialFeedMobile/Feed/UI/Views/Helpers/UIView+Shimmering.swift
@@ -7,44 +7,64 @@ import UIKit
 
 extension UIView {
     public var isShimmering: Bool {
-        get {
-            layer.mask?.animation(forKey: shimmerAnimationKey) != nil
-        } set {
+        set {
             if newValue {
                 startShimmering()
             } else {
                 stopShimmering()
             }
         }
-    }
 
-    private var shimmerAnimationKey: String {
-        "shimmer"
+        get {
+            layer.mask is ShimmeringLayer
+        }
     }
 
     private func startShimmering() {
-        let white = UIColor.white.cgColor
-        let alpha = UIColor.white.withAlphaComponent(0.7).cgColor
-        let width = bounds.width
-        let height = bounds.height
-
-        let gradient = CAGradientLayer()
-        gradient.colors = [alpha, white, alpha]
-        gradient.startPoint = CGPoint(x: 0.0, y: 0.4)
-        gradient.endPoint = CGPoint(x: 1.0, y: 0.6)
-        gradient.locations = [0.4, 0.5, 0.6]
-        gradient.frame = CGRect(x: -width, y: 0, width: width * 3, height: height)
-        layer.mask = gradient
-
-        let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.locations))
-        animation.fromValue = [0.0, 0.1, 0.2]
-        animation.toValue = [0.8, 0.9, 1.0]
-        animation.duration = 1
-        animation.repeatCount = .infinity
-        gradient.add(animation, forKey: shimmerAnimationKey)
+        layer.mask = ShimmeringLayer(size: bounds.size)
     }
 
     private func stopShimmering() {
         layer.mask = nil
+    }
+
+    private final class ShimmeringLayer: CAGradientLayer {
+        override nonisolated init() {
+            super.init()
+        }
+
+        override nonisolated init(layer: Any) {
+            super.init(layer: layer)
+        }
+
+        required nonisolated init?(coder: NSCoder) {
+            super.init(coder: coder)
+        }
+
+        private var observer: Any?
+
+        convenience init(size: CGSize) {
+            self.init()
+
+            let white = UIColor.white.cgColor
+            let alpha = UIColor.white.withAlphaComponent(0.75).cgColor
+
+            colors = [alpha, white, alpha]
+            startPoint = CGPoint(x: 0.0, y: 0.4)
+            endPoint = CGPoint(x: 1.0, y: 0.6)
+            locations = [0.4, 0.5, 0.6]
+            frame = CGRect(x: -size.width, y: 0, width: size.width * 3, height: size.height)
+
+            let animation = CABasicAnimation(keyPath: #keyPath(CAGradientLayer.locations))
+            animation.fromValue = [0.0, 0.1, 0.2]
+            animation.toValue = [0.8, 0.9, 1.0]
+            animation.duration = 1.25
+            animation.repeatCount = .infinity
+            add(animation, forKey: "shimmer")
+
+            self.observer = NotificationCenter.default.addObserver(of: UIApplication.shared, for: .willEnterForeground) { [weak self] _ in
+                self?.add(animation, forKey: "shimmer")
+            }
+        }
     }
 }
